5965a7eb81eb69df51fccb80b4cf8ea9b965c602
ParagShrivas <parag.shrivas24@gmail.com>
2026-06-27 02:58:47 +0530

    fix: disable job to test server startup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const app = require("./app");
-const startUpcomingNotificationMailJob = require("./jobs/upcomingNotificationMailJob");
+// const startUpcomingNotificationMailJob = require("./jobs/upcomingNotificationMailJob");
 
 const PORT = process.env.PORT || 1500;
-
 app.listen(PORT, () => {
-     console.log(`Server is running on port ${PORT}`);
-     startUpcomingNotificationMailJob();
+  console.log(`Server is running on port ${PORT}`);
+  // startUpcomingNotificationMailJob();
 });
